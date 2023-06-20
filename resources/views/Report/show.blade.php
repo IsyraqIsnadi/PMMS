d82adcb12e3371fd19eca06c97538c72848f47bf
IsyraqIsnadi <misyraq9@gmail.com>
2023-06-20 09:50:46 +0800

    report.show update

--- resources/views/Report/show.blade.php
+++ resources/views/Report/show.blade.php
@@ -16,6 +16,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" defer></script>
 
     <style>
         body {
@@ -48,7 +49,29 @@
         strong {
             font-weight: 600;
         }
+
+        .btn-download {
+            background-color: #4a5568;
+            color: #fff;
+            padding: 10px 20px;
+            border-radius: 4px;
+            text-decoration: none;
+            display: inline-block;
+        }
     </style>
+
+    <script>
+        function downloadAsPDF() {
+            // Get the report details container
+            const reportContainer = document.querySelector('.report-container');
+
+            // Convert the container to a PDF
+            html2pdf()
+                .set({ margin: 10, filename: 'report_details.pdf', image: { type: 'jpeg', quality: 0.98 } })
+                .from(reportContainer)
+                .save();
+        }
+    </script>
 </head>
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
@@ -60,26 +83,30 @@
         <div class="container">
             <h1>Report Details</h1>
 
-            <ul>
-                <li><strong>ID:</strong> {{ $report->report_id }}</li>
-                <li><strong>User ID:</strong> {{ $report->user_id }}</li>
-                <li><strong>Report Type:</strong> {{ $report->report_type }}</li>
-                <li><strong>Start Date:</strong> {{ $report->start_date }}</li>
-                <li><strong>End Date:</strong> {{ $report->end_date }}</li>
-                <li><strong>Status:</strong> {{ $report->status }}</li>
-                <li><strong>File Name:</strong> {{ $report->file_name }}</li>
-                <li><strong>Created Date:</strong> {{ $report->created_date }}</li>
-                <li>
-                    <strong>Roster:</strong>
-                    <ul>
-                        <li><strong>Name:</strong> {{ $report->roster->name }}</li>
-                        <li><strong>Start Time:</strong> {{ $report->roster->start_time }}</li>
-                        <li><strong>End Time:</strong> {{ $report->roster->end_time }}</li>
-                        <li><strong>Date:</strong> {{ $report->roster->date }}</li>
-                        <li><strong>Day:</strong> {{ $report->roster->day }}</li>
-                    </ul>
-                </li>
-            </ul>
+            <div class="report-container">
+                <ul>
+                    <li><strong>ID:</strong> {{ $report->report_id }}</li>
+                    <li><strong>User ID:</strong> {{ $report->user_id }}</li>
+                    <li><strong>Report Type:</strong> {{ $report->report_type }}</li>
+                    <li><strong>Start Date:</strong> {{ $report->start_date }}</li>
+                    <li><strong>End Date:</strong> {{ $report->end_date }}</li>
+                    <li><strong>Status:</strong> {{ $report->status }}</li>
+                    <li><strong>File Name:</strong> {{ $report->file_name }}</li>
+                    <li><strong>Created Date:</strong> {{ $report->created_date }}</li>
+                    <li>
+                        <strong>Roster:</strong>
+                        <ul>
+                            <li><strong>Name:</strong> {{ $report->roster->name }}</li>
+                            <li><strong>Start Time:</strong> {{ $report->roster->start_time }}</li>
+                            <li><strong>End Time:</strong> {{ $report->roster->end_time }}</li>
+                            <li><strong>Date:</strong> {{ $report->roster->date }}</li>
+                            <li><strong>Day:</strong> {{ $report->roster->day }}</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+            <a href="#" class="btn-download" onclick="downloadAsPDF()">Download PDF</a>
         </div>
     </div>
 </body>
